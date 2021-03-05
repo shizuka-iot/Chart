83fdfe647259857edd0f48b7b5d66aaeb9232ff5
shizuka-iot <tatakitubusu@gmail.com>
2021-03-05 15:26:44 +0900

    githubpages作成

--- index.php
+++ index.php
@@ -26,35 +26,18 @@ $sectorInfo = json_encode($sectInfo);
 
 <body class="center">
 	<header class="center container">
-		<h1 class="fs18 under-line"><?= h(SITE_TITLE)?></h1>
-
-		<h2>最初にグラフ描画のデータを二次元配列で用意します</h2>
-
-		<p>具体的には下記のようなものを用意してください
-		<br>
-			0番目の要素に扇（セクター）の色<br>
-			1番目の要素に項目名<br>
-			2番目の要素に量（パーセンテージでななく量)を指定してください<br>
-		</p>
-
-		<p>
-			$sectInfo = [<br>
-				["blue", "php", 299],<br>
-				["green", "pdo", 100],<br>
-				["red", "mysql", 49],<br>
-			];<br>
-		</p>
-
-		<h2>Circle.jsを読み込みます</h2>
-		<h2>新しくjsファイルを用意するかスクリプトタグでjsを記述していきます</h2>
+		<h1>サンプルページ</h1>
 	</header>
 
 	<main class="center">
 		<div class="container fs14 center">
-			<h2>キャンバス１</h2>
+			<h2>キャンバス1</h2>
 			<canvas id="can" width="800" height="480"></canvas>
+			<h2>キャンバス2</h2>
 			<canvas id="can2" width="800" height="480"></canvas>
+			<h2>キャンバス3</h2>
 			<canvas id="can4" width="800" height="600"></canvas>
+			<h2>キャンバス4</h2>
 			<canvas id="can3" width="800" height="600"></canvas>
 		</div>
 	</main>
@@ -66,7 +49,7 @@ $sectorInfo = json_encode($sectInfo);
 	<script>
 		const arr = <?php echo $sectorInfo; ?>;
 	</script>
-	<script src="circle.js"></script>
+	<script src="Circle.js"></script>
 	<script src="main.js"></script>
 </body>
 </html>
